a1b76a005de3771ac2b76292d75e3befde29a18d
FaridPutraaa <faridputraaa@gmail.com>
2021-03-27 12:45:25 +0700

    commit

---
+++ Python_Data_Professional_Beginner_Part2/String_Manipulation_dengan_Python/Menentukan_String_Awal_Akhir.py
@@ -0,0 +1,10 @@
+# Fitur .startswith()
+print(">>> Fitur .startswith()")
+teks = "Apel malang adalah apel termanis dibanding apel-apel lainnya"
+print(teks.startswith("Apel"))
+print(teks.startswith("apel"))
+
+# Fitur .endswith()
+print(">>> Fitur .endswith()")
+print(teks.endswith("lainnya"))
+print(teks.endswith("apel"))
---
+++ Python_Data_Professional_Beginner_Part2/String_Manipulation_dengan_Python/Menentukan_posisi_dan_jml_substring.py
@@ -0,0 +1,10 @@
+teks = "Apel malang adalah apel termanis dibanding apel-apel lainnya"
+# Fitur .find()
+print(">>> Fitur .find()")
+print(teks.find("Apel"))
+print(teks.find("malang"))
+
+# Fitur .count()
+print(">>> Fitur .count()")
+kemunculan_kata_apel = teks.count("apel")
+print(kemunculan_kata_apel)
