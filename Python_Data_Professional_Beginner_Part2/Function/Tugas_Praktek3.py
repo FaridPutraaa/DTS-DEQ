2cea1e1bfbc5e88eb51a4df724db03e87ba50909
FaridPutraaa <faridputraaa@gmail.com>
2021-03-28 19:44:35 +0700

    co

--- Python_Data_Professional_Beginner_Part2/Function/Tugas_Praktek3.py
+++ Python_Data_Professional_Beginner_Part2/Function/Tugas_Praktek3.py
@@ -24,12 +24,12 @@ def hitung_standar_deviasi(data):
 # Definisikan fungsi untuk menghitung rata-rata dan standar deviasi
 # setiap kolom pada tabel_properti yang diberikan oleh key dict.
 def deskripsi_properti(tabel):
-    for key in ___:
-        print('Rata-rata ' + ___ + ':')
-        print(___) 
-        print('Standar deviasi ' + ___ + ':')
-        print(___)
+    for key in tabel.keys():
+        print('Rata-rata ' + key + ':')
+        print(hitung_rata_rata(tabel[key])) 
+        print('Standar deviasi ' + key + ':')
+        print(hitung_standar_deviasi(tabel[key]))
         print('')
 # Panggil fungsi deskripsi_properti untuk menghitung rata-rata 
 # dan standar deviasi setiap kolom pada tabel_properti
-deskripsi_properti(___)
+deskripsi_properti(tabel_properti)
