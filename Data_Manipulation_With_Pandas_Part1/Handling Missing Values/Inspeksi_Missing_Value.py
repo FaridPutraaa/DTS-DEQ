9708695c1fb3ab2bc1c726b9b800e043092983a4
FaridPutraaa <faridputraaa@gmail.com>
2021-04-19 23:25:23 +0700

    commit

---
+++ Data_Manipulation_With_Pandas_Part1/Handling Missing Values/Inspeksi_Missing_Value.py
@@ -0,0 +1,8 @@
+import pandas as pd
+# Baca file "public data covid19 jhu csse eu.csv"
+df = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/CHAPTER%204%20-%20missing%20value%20-%20public%20data%20covid19%20.csv")
+# Cetak info dari df
+print(df.info())
+# Cetak jumlah missing value di setiap kolom
+mv = df.isna().sum()
+print("\nJumlah Missing value per kolom:\n", mv)
